30b075be7d4a375bdf995253e07f85caa6b99b76
Jorgiba <128039685+Jorgiba@users.noreply.github.com>
2024-03-06 19:16:18 +0100

    Update menu.java

--- menu.java
+++ menu.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 
 public class Menu{
-    private Integer nMenus;
+    private static Integer nMenus;
     private Integer id;
     private Set<Plato> platos = new HashSet<>();
 
